614d8b214013353df1b48577206de17240c87fa3
Julia <droppingboxes@tuta.io>
2017-12-28 21:31:54 -0600

    🎉 first
    flask app one

---
+++ one.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Birdie was Here"
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ one.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, "/var/www/one")
+from one import app as application
